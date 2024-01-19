10292da18101d19cc85eae9fe74bc4a9919dd55a
kosdasAslıhan <kosdasaslihan@gmail.com>
2024-01-19 14:42:37 +0300

    Practice

---
+++ src/main/java/Day1/C02_Ortalama.java
@@ -0,0 +1,26 @@
+package Day1;
+
+public class C02_Ortalama {
+
+    public static void main(String[] args) {
+
+        double num1 = 12.4;
+        double num2 = 25.8;
+        double num3 = 800;
+        double num4 = 8.2;
+        double num5 = 32.6;
+
+        //Yukaridaki sayilarin ortalamasini hesaplayan bir kod yaziniz
+        //Ortalama hesaplama: Tum sayilari topla, sayi adedine bol.
+
+        //1. yol
+        double toplam = num1+num2+num3+num4+num5;
+        System.out.println("toplam = " + toplam);
+
+        System.out.println("Ortalama: "+ toplam/5);
+
+        //2. yol
+        System.out.println("Ortalama2:"+ (num1+num2+num3+num4+num5)/5);
+
+    }
+}
